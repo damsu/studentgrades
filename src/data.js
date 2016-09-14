af4d33bd6c7bda26edc699ecd19528a2dc0f9ea4
damsu <damorizur@gmail.com>
2016-09-14 14:08:38 +0300

    test on grade fucntion

--- src/data.js
+++ src/data.js
@@ -135,10 +135,10 @@ exports.updateCourseById = function (id, newcontent) {
 
 //Grades
 exports.getAllGradesByStudentId = function (id) {
-    //return gradeData.filter(function (grade) {
-    //    if (grade.studentId == id) {
-    //        return grade;
-    //    }
-    //});
-    return gradeData;
+    return gradeData.filter(function (grade) {
+        if (grade.studentId == id) {
+            return grade;
+        }
+    });
+    //return gradeData;
 }
